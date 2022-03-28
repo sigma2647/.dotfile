aa2f86ed149a9615bca0a9d96d2b1f4cd6b2928b
sigma2647_macmini <1169446204@qq.com>
2022-03-29 03:28:35 +0800

    add

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -21,8 +21,10 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "Iosevka SS04" :size 16 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "Iosevka SS04" :size 16))
+;;(setq doom-font (font-spec :family "Iosevka SS04" :size 16 :weight 'regular)
+;;      doom-variable-pitch-font (font-spec :family "Iosevka SS04" :size 16))
+(setq doom-font (font-spec :family "MesloLGS NF" :size 16 :weight 'regular)
+      doom-variable-pitch-font (font-spec :family "MesloLGS NF" :size 16))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
--- emacs/.doom.d/packages.el
+++ emacs/.doom.d/packages.el
@@ -1,5 +1,12 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
+;;; 这里用来安装插件
+;;; cmd:doom install
+;;; doom sync
+;;; doom upgrade
+
+
+
 
 ;; To install a package with Doom you must declare them here and run 'doom sync'
 ;; on the command line, then restart Emacs for the changes to take effect -- or
