3123c2dd423bf17be6588e99765fe42283b646e9
sigma2647_macmini <1169446204@qq.com>
2022-04-21 02:28:49 +0800

    add ipython

---
+++ ipython/.ipython/profile_default/startup/pyflyby_config.py
@@ -0,0 +1,33 @@
+
+"""================================================================
+此文件为pyflyby的配置文件
+~/.ipython/profile_default/startup
+"""
+
+from IPython import get_ipython
+import subprocess
+
+import pandas as pd
+import numpy as np
+import visidata as vd
+
+vd = vd.view_pandas
+
+# ipython = get_ipython()
+
+# try:
+    # ipython.magic("load_ext pyflyby")
+# except ModuleNotFoundError:
+    # print("pyflyby 未安装, 正在安装...")
+    # subprocess.Poen(
+            # ["pip", "install", "pyflyby"],
+            # stdout=subprocess.DEVNULL,
+            # stdourr=subprocess.DEVNULL,
+    # ).wait()
+    # ipython.magic("load_ext pyflyby")
+    # subprocess.Popen(
+            # ["pip", "install", "ipython-sql"],
+            # stdout=subprocess.DEVNULL,
+            # stdourr=subprocess.DEVNULL,
+    # )
+
