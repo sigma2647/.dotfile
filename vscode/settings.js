1db880ff58023678ebb3178a99788fbe13e44811
sigma2647_macmini <1169446204@qq.com>
2022-04-26 02:58:32 +0800

    add vscode

---
+++ vscode/settings.js
@@ -0,0 +1,21 @@
+{
+    "editor.inlineSuggest.enabled": true,
+    "github.copilot.enable": {
+        "*": true,
+        "yaml": false,
+        "plaintext": false,
+        "markdown": false,
+        "python": true
+    },
+    "editor.fontFamily": "MesloLGLDZ Nerd Font Mono",
+    "editor.fontSize": 15,
+    "terminal.integrated.inheritEnv": false,
+    "files.autoSave": "onWindowChange",
+    "window.zoomLevel": 1,
+    "editor.cursorBlinking": "phase",
+    "editor.cursorSmoothCaretAnimation": true,
+    "editor.bracketPairColorization.enabled": true,
+    "workbench.colorTheme": "Atom One Dark",
+    "vscode-neovim.neovimInitVimPaths.darwin": "/Users/lawrencexing/.config/nvim/init.lua",
+    "vscode-neovim.neovimExecutablePaths.darwin": "/opt/homebrew/bin/nvim"
+}
