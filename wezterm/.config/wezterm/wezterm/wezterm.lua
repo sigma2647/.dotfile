19e437d11180aecdd4f7d238095c3b67e7a16ef5
sigma2647 <1169446204@qq.com>
2024-03-06 07:44:05 +0800

    add wezterm

---
+++ wezterm/.config/wezterm/wezterm/wezterm.lua
@@ -0,0 +1,6 @@
+local wezterm = require("wezterm")
+local config = {
+	font_size = 40,
+}
+
+return config
---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,34 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+local config = {
+	font_size = 16,
+	color_scheme = "Catppuccin Mocha",
+	font = wezterm.font("JetBrainsMonoNL Nerd Font", { weight = "Regular" }),
+	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
+	window_decorations = "RESIZE",
+	show_new_tab_button_in_tab_bar = false,
+	window_background_opacity = 0.9,
+	macos_window_background_blur = 70,
+	adjust_window_size_when_changing_font_size = false,
+	text_background_opacity = 0.9,
+
+	window_padding = {
+		left = 20,
+		right = 20,
+		top = 20,
+		bottom = 20,
+	},
+}
+
+-- This will hold the configuration.
+-- local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.color_scheme = "AdventureTime"
+
+-- and finally, return the configuration to wezterm
+return config
