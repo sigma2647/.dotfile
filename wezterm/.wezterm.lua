31d23182a8b59f78a19bcb6af45385119de587fd
sigma2647 <1169446204@qq.com>
2024-03-11 07:21:59 +0800

    upda

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -15,10 +15,10 @@ local config = {
 	text_background_opacity = 0.9,
 
 	window_padding = {
-		left = 20,
-		right = 20,
-		top = 20,
-		bottom = 20,
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
 	},
 	-- default_prog = { "/usr/local/bin/tmux" }, -- 默认启动方式 "zsh" "bash" "tmux"
 }
