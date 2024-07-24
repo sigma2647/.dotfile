1d48bfe2f6763048667d5e4aa2a0c9afd277525b
sigma2647 <1169446204@qq.com>
2024-07-24 20:45:33 +0800

    update powershell

--- powershell/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ powershell/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,20 @@
 Set-PSReadLineOption -EditMode Emacs
+
+Set-Alias -Name open -Value explorer.exe
+
+Set-Alias -Name g -Value lazygit
+
+Set-Alias -Name l -Value lfcd
+
+Set-Alias -Name v -Value nvim
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -ShowToolTips
+
+
+function lfcd {
+    $lastDir = & lf -print-last-dir $args
+    if ($lastDir -ne $null) {
+        Set-Location -Path $lastDir
+    }
+}
