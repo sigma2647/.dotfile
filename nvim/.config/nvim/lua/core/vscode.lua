340080ee71a7532dffab171f8871c84826aa5c90
sigma2647 <1169446204@qq.com>
2025-01-05 00:42:32 +0800

    upda

--- nvim/.config/nvim/lua/core/vscode.lua
+++ nvim/.config/nvim/lua/core/vscode.lua
@@ -1,142 +1,75 @@
-local disable_providers = function()
-    local default_providers = {
-        "node",
-        "perl",
-        "python3",
-        "ruby",
-    }
+-- core/vscode.lua
 
-    for _, provider in ipairs(default_providers) do
-        vim.g["loaded_" .. provider .. "_provider"] = 0
-    end
+local M = {}
+
+-- 键位映射辅助函数
+local function map(mode, lhs, rhs, opts)
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
 end
 
-local leader_map = function()
-    vim.api.nvim_set_keymap("n", "<Space>", "", { noremap = true })
-    vim.api.nvim_set_keymap("x", "<Space>", "", { noremap = true })
-    vim.g.maplocalleader = " "
-    vim.g.mapleader = " "
+-- VSCode 特定配置
+function M.setup()
+    -- 基础设置
+    vim.opt.clipboard:append("unnamedplus") -- 使用系统剪贴板
+    vim.opt.ignorecase = true              -- 搜索时忽略大小写
+    vim.opt.smartcase = true               -- 搜索时智能大小写
+    vim.opt.scrolloff = 5                  -- 保持光标上下文可见行数
+    
+
+    vim.g.mapleader = " "                                   -- 设置leader键为空格
+    -- 常用键位映射
+    -- 窗口导航
+    map('n', '<C-j>', [[<Cmd>call VSCodeNotify('workbench.action.navigateDown')<CR>]])
+    map('n', '<C-k>', [[<Cmd>call VSCodeNotify('workbench.action.navigateUp')<CR>]])
+    map('n', '<C-h>', [[<Cmd>call VSCodeNotify('workbench.action.navigateLeft')<CR>]])
+    map('n', '<C-l>', [[<Cmd>call VSCodeNotify('workbench.action.navigateRight')<CR>]])
+    
+    -- 文件操作
+    map('n', '<leader>f', [[<Cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>]])
+    map('n', '<leader>s', [[<Cmd>call VSCodeNotify('workbench.action.files.save')<CR>]])
+    
+    -- 代码操作
+    map('n', 'gd', [[<Cmd>call VSCodeNotify('editor.action.revealDefinition')<CR>]])
+    map('n', 'gr', [[<Cmd>call VSCodeNotify('editor.action.goToReferences')<CR>]])
+    map('n', '<leader>rn', [[<Cmd>call VSCodeNotify('editor.action.rename')<CR>]])
+    map('n', '<leader>ca', [[<Cmd>call VSCodeNotify('editor.action.quickFix')<CR>]])
+    
+    -- 搜索相关
+    map('n', '<leader>fw', [[<Cmd>call VSCodeNotify('workbench.action.findInFiles')<CR>]])
+    map('n', '<leader>r', [[<Cmd>call VSCodeNotify('editor.action.startFindReplaceAction')<CR>]])
+    
+    -- 注释
+    map('n', 'gcc', [[<Cmd>call VSCodeNotify('editor.action.commentLine')<CR>]])
+    map('v', 'gc', [[<Cmd>call VSCodeNotify('editor.action.commentLine')<CR>]])
+    
+    -- 面板操作
+    map('n', '<leader>e', [[<Cmd>call VSCodeNotify('workbench.action.toggleSidebarVisibility')<CR>]])
+
+    map('n', '<leader>t', [[<Cmd>call VSCodeNotify('workbench.action.terminal.toggleTerminal')<CR>]])
+    
+    -- 缩进操作保持 Visual 模式
+    map('v', '<', '<gv')
+    map('v', '>', '>gv')
+    
+    -- 移动行
+    map('n', '<A-j>', [[<Cmd>call VSCodeNotify('editor.action.moveLinesDownAction')<CR>]])
+    map('n', '<A-k>', [[<Cmd>call VSCodeNotify('editor.action.moveLinesUpAction')<CR>]])
+    map('v', '<A-j>', [[<Cmd>call VSCodeNotify('editor.action.moveLinesDownAction')<CR>]])
+    map('v', '<A-k>', [[<Cmd>call VSCodeNotify('editor.action.moveLinesUpAction')<CR>]])
+    
+    -- 折叠
+    map('n', 'za', [[<Cmd>call VSCodeNotify('editor.toggleFold')<CR>]])
+    map('n', 'zR', [[<Cmd>call VSCodeNotify('editor.unfoldAll')<CR>]])
+    map('n', 'zM', [[<Cmd>call VSCodeNotify('editor.foldAll')<CR>]])
+
+    map("n", "H", "^")                -- 快速到行首
+    map("n", "L", "$")                -- 快速到行尾
+    map("v", "H", "^")                -- 可视模式行首
+    map("v", "L", "$")                -- 可视模式行尾
+    -- require("core.keymaps")
+
+
 end
-
-local clipboard_config = function()
-    vim.g.clipboard = {
-        name = "xsel",
-        copy = {
-            ["+"] = "xsel --nodetach -i -b",
-            ["*"] = "xsel --nodetach -i -p",
-        },
-        paste = {
-            ["+"] = "xsel -o -b",
-            ["*"] = "xsel -o -p",
-        },
-        cache_enabled = 1,
-    }
-end
-
-local keymapping = function()
-    local opts = { noremap = true, silent = true }
-
-    local keymap = vim.keymap.set
-
-    -- Diferentes formas de guardar y salir
-    -- Moverse de forma normal en lineas largas con wrap
-    keymap("n", "k", "(v:count == 0 ? 'gk' : 'k')", { silent = true, expr = true })
-    keymap("n", "j", "(v:count == 0 ? 'gj' : 'j')", { silent = true, expr = true })
-    keymap("n", "<Down>", "(v:count == 0 ? 'gj' : 'j')", { silent = true, expr = true })
-    keymap("n", "<Up>", "(v:count == 0 ? 'gk' : 'k')", { silent = true, expr = true })
-    -- MODO VISUAL
-    -- Mejor indentacion
-    keymap("v", "<", "<gv", opts)
-    keymap("v", ">", ">gv", opts)
-    -- Moverse de forma normal en lineas largas con wrap
-    keymap("v", "j", "(v:count == 0 ? 'gj' : 'j')", { silent = true, expr = true })
-    keymap("v", "k", "(v:count == 0 ? 'gk' : 'k')", { silent = true, expr = true })
-    keymap("v", "<Down>", "(v:count == 0 ? 'gj' : 'j')", { silent = true, expr = true })
-    keymap("v", "<Up>", "(v:count == 0 ? 'gk' : 'k')", { silent = true, expr = true })
-    vim.cmd([[
-        xmap gc  <Plug>VSCodeCommentary
-        nmap gc  <Plug>VSCodeCommentary
-        omap gc  <Plug>VSCodeCommentary
-        nmap gcc <Plug>VSCodeCommentaryLine
-        nnoremap <silent> <C-j> :call VSCodeNotify('workbench.action.navigateDown')<CR>
-        xnoremap <silent> <C-j> :call VSCodeNotify('workbench.action.navigateDown')<CR>
-        nnoremap <silent> <C-k> :call VSCodeNotify('workbench.action.navigateUp')<CR>
-        xnoremap <silent> <C-k> :call VSCodeNotify('workbench.action.navigateUp')<CR>
-        nnoremap <silent> <C-h> :call VSCodeNotify('workbench.action.navigateLeft')<CR>
-        xnoremap <silent> <C-h> :call VSCodeNotify('workbench.action.navigateLeft')<CR>
-        nnoremap <silent> <C-l> :call VSCodeNotify('workbench.action.navigateRight')<CR>
-        xnoremap <silent> <C-l> :call VSCodeNotify('workbench.action.navigateRight')<CR>
-    ]])
-end
-
-local plugins = function()
-    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-    if not vim.loop.fs_stat(lazypath) then
-        vim.fn.system({
-            "git",
-            "clone",
-            "--filter=blob:none",
-            "https://github.com/folke/lazy.nvim.git",
-            "--branch=stable",
-            lazypath,
-        })
-    end
-    vim.opt.rtp:prepend(lazypath)
-
-    require("lazy").setup({
-        {
-            "kylechui/nvim-surround",
-            version = "*",
-            keys = { "cs", "ys", "ds" },
-            config = true,
-        },
-        {
-            "phaazon/hop.nvim",
-            cmd = "HopWord",
-            keys = { { "ñ", "<cmd>HopWord<cr>" } },
-            opts = {
-                keys = "etovxqpdygfblzhckisuran",
-            },
-        },
-        {
-            "nvim-lua/plenary.nvim",
-            event = "VeryLazy",
-        },
-    })
-end
-
-local colors = function()
-    vim.api.nvim_set_hl(0, "HopUnmatched", { fg = "#6c7086" })
-    vim.api.nvim_set_hl(0, "HopNextKey", { fg = "#f9e2af", bold = true })
-    vim.api.nvim_set_hl(0, "HopNextKey1", { fg = "#89b4fa", bold = true })
-    vim.api.nvim_set_hl(0, "HopNextKey2", { fg = "#94e2d5", bold = true, italic = true })
-end
-
-local autocmds = function()
-    local autocmd = vim.api.nvim_create_autocmd
-
-    autocmd("TextYankPost", {
-        callback = function()
-            vim.highlight.on_yank()
-        end,
-    })
-end
-
-local load_core = function()
-    -- disable_providers()
-    -- leader_map()
-    -- clipboard_config()
-    -- keymapping()
-    -- plugins()
-    -- colors()
-    -- autocmds()
-
-    -- require("core.options")
-    require("core.keymaps")
-    -- vim.keymap.set("n", "H", "^")
-    -- vim.keymap.set("n", "L", "$")
-    -- vim.keymap.set("v", "H", "^")
-    -- vim.keymap.set("v", "L", "$")
-end
-
-load_core()
+return M
