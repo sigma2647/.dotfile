9694388aebd4c665ebba55b4dfde5c424c4a5f4c
sigma2647 <1169446204@qq.com>
2025-01-13 19:40:10 +0800

    upa

--- powershell/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ powershell/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -27,7 +27,7 @@ function lfcd {
 
 #f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
 
-Import-Module -Name Microsoft.WinGet.CommandNotFound
+# Import-Module -Name Microsoft.WinGet.CommandNotFound
 #f45873b3-b655-43a6-b217-97c00aa0db58
 
 $env:POWERSHELL_UPDATECHECK = 'Off'
